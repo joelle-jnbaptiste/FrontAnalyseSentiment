3c6f1944d67faac2ffd8ab90b025171bab0ccb84
joelle-jnbaptiste <joelle.jnbaptiste@gmail.com>
2025-04-17 11:45:54 +0200

    pep8

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import requests
 import urllib3
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 st.title("Analyse de Sentiments")
@@ -21,9 +22,9 @@ if st.button("Analyser le sentiment"):
         st.warning("Veuillez entrer un message.")
     else:
         try:
-            
+
             response = requests.post(
-                "https://web-api-sentiment.graysky-45e01b24.francecentral.azurecontainerapps.io/predict", 
+                "https://web-api-sentiment.graysky-45e01b24.francecentral.azurecontainerapps.io/predict",
                 json={"text": user_input},
                 verify=False
             )
@@ -31,7 +32,8 @@ if st.button("Analyser le sentiment"):
             if response.status_code == 200:
                 result = response.json()
                 st.session_state.user_text = user_input
-                st.session_state.prediction_result = result.get("prediction", 0)
+                st.session_state.prediction_result = result.get(
+                    "prediction", 0)
             else:
                 st.error(f"Erreur de l'API : {response.status_code}")
 
@@ -42,7 +44,8 @@ if st.button("Analyser le sentiment"):
 if st.session_state.prediction_result is not None:
     sentiment = "✅ Sentiment positif" if st.session_state.prediction_result == 1 else "❌ Sentiment négatif"
     st.subheader(sentiment)
-    feedback = st.radio("Cette prédiction vous semble-t-elle correcte ?", ["Oui", "Non"])
+    feedback = st.radio(
+        "Cette prédiction vous semble-t-elle correcte ?", ["Oui", "Non"])
     send_feedback = st.button("Envoyer le feedback")
 
     if send_feedback:
@@ -63,7 +66,9 @@ if st.session_state.prediction_result is not None:
                 message = response.json().get("message", "Feedback envoyé.")
                 st.success(message)
             else:
-                st.error(f"Erreur lors de l’envoi du feedback : {response.status_code}")
+                st.error(
+                    f"Erreur lors de l’envoi du feedback : {response.status_code}")
 
         except Exception as e:
-            st.error(f"Une erreur est survenue pendant l’envoi du feedback : {e}")
+            st.error(
+                f"Une erreur est survenue pendant l’envoi du feedback : {e}")
